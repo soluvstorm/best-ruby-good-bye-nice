6f7f6ae7300d7a68778fca59e3df62831dcc9282
Hien <1559014@itec.hcmus.edu.vn>
2020-04-20 03:18:37 +0700

    update code

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  
+  def hello
+    render html: "hello world !"
+  end
+
+  def goodbye
+    render html: "good bye ! "
+  end
+  
+
+end
